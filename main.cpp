b8d4cbafcd2cf9ae7a3d2d6d126574b53414c63c
voyadger01 <voyadgerkx@gmail.com>
2025-12-05 12:39:42 +0300

    operators add

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
-
-struct p_t
+namespace topit
 {
-  int x, y;
-};
-
-struct IDraw
-{
-  virtual p_t begin() const = 0;
-  virtual p_t next(p_t) const = 0; 
-  virtual ~IDraw();
-};
+  struct p_t
+  {
+    int x, y;
+  };
+  bool operator==(p_t, p_t);
+  bool operator!=(p_t, p_t);
+  struct f_t
+  {
+    p_t aa, bb;
+  };
+  
+  struct IDraw
+  {
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0; 
+    virtual ~IDraw();
+  };
+}
 
 int main()
 {
+  using topit::p_t;
+  p_t a{1, 0}, b{1, 0};
+  std::cout << (a == b) << '\n';
+}
 
+bool topit::operator==(p_t a, p_t b)
+{
+  return a.x == b.x && a.y == b.y;
+}
+
+bool topit::operator!=(p_t a, p_t b)
+{
+  return !(a == b);
 }
