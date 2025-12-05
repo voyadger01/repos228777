cacad1771bd29d1c53aca33c88810b2f71ac0015
voyadger01 <voyadgerkx@gmail.com>
2025-12-05 12:31:28 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct p_t
+{
+  int x, y;
+};
+
+struct IDraw
+{
+  virtual p_t begin() const = 0;
+  virtual p_t next(p_t) const = 0; 
+  virtual ~IDraw();
+};
+
+int main()
+{
+
+}
